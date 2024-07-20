ceef225eddf921c92830bce839108a4208ed1dec
Trung Vo <trungk18@gmail.com>
2024-07-20 16:49:14 +0800

    feat: update root layout

--- music-react/src/routes/Root.tsx
+++ music-react/src/routes/Root.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function Root() {
   return (
-    <div className='max-w-6xl mx-auto'>
+    <div className='container mx-auto'>
       <Outlet></Outlet>
     </div>
   );

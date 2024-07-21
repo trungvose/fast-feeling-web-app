73ec2b3231cc032411e0191919e9f0c89afa3c78
Trung Vo <trungk18@gmail.com>
2024-07-21 17:35:05 +0800

    feat: add cat routes

---
+++ music-rails/app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def cat1
+  end
+
+  def cat2
+  end
+end
--- music-rails/config/routes.rb
+++ music-rails/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#cat1' # Set cat1 as the default home page
+  
   get 'playlists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
@@ -7,6 +9,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "playlists" => "playlists#index"
   get 'playlists/:id', to: 'playlists#detail', as: 'playlist_detail'
+
+  # Cat
+  get 'cat1', to: 'pages#cat1'
+  get 'cat2', to: 'pages#cat2'
   
   # Defines the root path route ("/")
   # root "posts#index"

7523b16508da7fca8253b13bf1f01c385334d2ad
Trung Vo <trungk18@gmail.com>
2024-10-11 17:34:12 +0800

    feat: useQuery staleTime and initialData

--- music-react/src/hooks/use-fetch-vehicle-query.tsx
+++ music-react/src/hooks/use-fetch-vehicle-query.tsx
@@ -1,7 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { fetchVehicle } from '../api/fetchVehcile';
+import { Vehicle } from '../types/vehicle';
 
 export const useFetchVehicle = (vehicleId: string | undefined) => {
+  const queryClient = useQueryClient();
   return useQuery({
     queryKey: ['vehicle', vehicleId],
     queryFn: () => fetchVehicle(vehicleId, {
@@ -9,5 +11,17 @@ export const useFetchVehicle = (vehicleId: string | undefined) => {
         delay: '1500',
       },
     }),
+    staleTime: 2 * 60 * 1000,
+    initialData: () => {
+      let data;
+      const queries = queryClient.getQueriesData<Vehicle[]>({
+        queryKey: ['vehicles'],
+      });
+      if (queries[0]) {
+        const [, vehicles] = queries[0];
+        data = vehicles?.find((vehicle) => `${vehicle.id}` === vehicleId);
+      }
+      return data;
+    },
   });
 };

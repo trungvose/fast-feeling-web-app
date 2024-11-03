dbd10f863ee442aa05f31e954e0420e3c3050cd7
Trung Vo <trungk18@gmail.com>
2024-11-03 14:54:56 +0800

    feat: demo5 with useQuery initial data and staleTime, fetch delay 1500ms

--- music-react/src/hooks/use-fetch-vehicle-query.tsx
+++ music-react/src/hooks/use-fetch-vehicle-query.tsx
@@ -1,13 +1,28 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { fetchVehicle } from '../api/fetchVehicle';
+import { Vehicle } from '../types/vehicle';
 
 export const useFetchVehicle = (vehicleId: string | undefined) => {
+  const queryClient = useQueryClient();
   return useQuery({
     queryKey: ['vehicle', vehicleId],
-    queryFn: () => fetchVehicle(vehicleId, {
-      headers: {
-        delay: '1500',
-      },
-    }),
+    queryFn: () =>
+      fetchVehicle(vehicleId, {
+        headers: {
+          delay: '1500',
+        },
+      }),
+    initialData: () => {
+      let data;
+      const queries = queryClient.getQueriesData<Vehicle[]>({
+        queryKey: ['vehicles'],
+      });
+      if (queries[0]) {
+        const [, vehicles] = queries[0];
+        data = vehicles?.find((vehicle) => `${vehicle.id}` === vehicleId);
+      }
+      return data;
+    },
+    staleTime: 2 * 60 * 1000,
   });
 };

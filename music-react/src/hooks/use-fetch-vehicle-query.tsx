0eb63ef14dfd451712115b2b9ed7214e7c048c56
Trung Vo <trungk18@gmail.com>
2024-11-03 13:28:20 +0800

    feat: demo4 with useQuery, fetch delay 1500ms

--- music-react/src/hooks/use-fetch-vehicle-query.tsx
+++ music-react/src/hooks/use-fetch-vehicle-query.tsx
@@ -1,9 +1,7 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { fetchVehicle } from '../api/fetchVehicle';
-import { Vehicle } from '../types/vehicle';
 
 export const useFetchVehicle = (vehicleId: string | undefined) => {
-  const queryClient = useQueryClient();
   return useQuery({
     queryKey: ['vehicle', vehicleId],
     queryFn: () => fetchVehicle(vehicleId, {
@@ -11,17 +9,5 @@ export const useFetchVehicle = (vehicleId: string | undefined) => {
         delay: '1500',
       },
     }),
-    staleTime: 2 * 60 * 1000,
-    initialData: () => {
-      let data;
-      const queries = queryClient.getQueriesData<Vehicle[]>({
-        queryKey: ['vehicles'],
-      });
-      if (queries[0]) {
-        const [, vehicles] = queries[0];
-        data = vehicles?.find((vehicle) => `${vehicle.id}` === vehicleId);
-      }
-      return data;
-    },
   });
 };
